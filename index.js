491472ba54fc4987865c3ae40eccab9c09bd09f9
Tazim212 <iftekhartazim21@gmail.com>
2026-07-13 21:14:19 +0600

    service center get query done

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express"
+const app = express()
+import {MongoClient} from'mongodb';
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const client = new MongoClient(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-eqifd2k-shard-00-00.tbmejyb.mongodb.net:27017,ac-eqifd2k-shard-00-01.tbmejyb.mongodb.net:27017,ac-eqifd2k-shard-00-02.tbmejyb.mongodb.net:27017/?ssl=true&replicaSet=atlas-bvjx8p-shard-0&authSource=admin&appName=Cluster0`);
+
+
+export async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("You successfully connected to MongoDB!");
+
+    const zapDB = client.db("zapDB")
+    const serviceCenters = zapDB.collection("serviceCenters")
+
+
+    app.get('/servicecenter', async(req, res) =>{
+        const cursor = serviceCenters.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    
+    return client;
+  } catch (err) {
+    console.dir(err);
+  }
+}
+// Call this only when your application terminates
+export async function disconnectFromMongoDB() {
+//   await client.close();
+}
+
+connectToMongoDB()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
